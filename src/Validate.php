0c2bbf8031ab38df0f6f16d8998cdb63dc4e7864
jo <jo@surikat.pro>
2016-12-08 17:48:22 +0100

    initial commit - bridge object

---
+++ src/Validate.php
@@ -0,0 +1,24 @@
+<?php
+namespace FoxORM\Validate;
+use Respect\Validation\Validator;
+use Respect\Validation\Factory;
+use Respect\Validation\Exceptions\ComponentException;
+class Validate{
+	protected $factory;
+	function __construct(){
+		$this->factory = new Factory();
+		$this->factory->prependRulePrefix(__NAMESPACE__.'\\Rules');
+	}
+	function __call($method, $arguments){
+		$validator = new Validator();
+		return $validator->addRule($this->buildRule($ruleSpec, $arguments));
+	}
+	function buildRule($ruleSpec, $arguments = []){
+        try {
+            return $this->factory->rule($ruleSpec, $arguments);
+        }
+        catch (\Exception $exception) {
+            throw new ComponentException($exception->getMessage(), $exception->getCode(), $exception);
+        }
+    }
+}
