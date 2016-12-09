eda04cd9bb34d1274800bb61a6762401b0e25d3a
jo <jo@surikat.pro>
2016-12-09 12:50:02 +0100

    clean

--- src/Rules/NotContains.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace FoxORM\Validate\Rules;
-use Respect\Validation\Rules\Contains;
-class NotContains extends Contains{
-    function validate($input){
-		return !parent::validate($input);
-    }
-}
