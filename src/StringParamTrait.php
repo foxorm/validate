0a0289bae1452afa1ef87aa95968c99524ad88ab
jo <jo@surikat.pro>
2016-12-09 12:58:41 +0100

    typo fix

--- src/StringParamTrait.php
+++ src/StringParamTrait.php
@@ -8,7 +8,7 @@ trait StringParamTrait{
 			if(strpos($rule,':')){
 				$x2 = explode(':',$rule);
 				$params = explode(',',array_pop($x2));
-				$name = count($params)==1?$x2[0]:$x2;
+				$name = count($x2)==1?$x2[0]:$x2;
 				array_unshift($params,$name);
 				$rules[] = $params;
 			}
