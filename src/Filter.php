8e35c6e96c2c8634f48dad3fdd8283f1db9f9ffc
jo <jo@surikat.pro>
2016-12-10 22:24:59 +0100

    add filter on __call

--- src/Filter.php
+++ src/Filter.php
@@ -7,4 +7,13 @@ class Filter{
 		$filter->addFilters($filterSet);
 		return $filter;
 	}
+	function __call($method,$arguments){
+		$filter = new FilterSet();
+		$filterResource = $filter->getFilterResource();
+		$value = array_pop($arguments);
+		call_user_func_array([$filterResource,$method],$arguments);
+		$result = $filter->filter([$value]);
+		$result = current($result);
+		return $result;
+	}
 }
